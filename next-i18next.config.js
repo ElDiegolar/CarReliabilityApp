7b8948360ab9640c80992086e3c94dacd4f9dd1b
Lorenzo La Gorce <lorenzo.g@horizon-web.io>
2025-04-05 15:57:49 +0200

    Refactor next-i18next configuration by removing non-standard options and adding debugging support for development

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,10 +2,21 @@
 module.exports = {
     i18n: {
       defaultLocale: 'en',
-      locales: ['en', 'es', 'fr', 'de'], // English, Spanish, French, German
+      locales: ['en', 'es', 'fr', 'de'],
     },
-    fallbackLng: {
-      default: ['en'],
-    },
-    nonExplicitSupportedLngs: true,
+    // Remove these non-standard options
+    // fallbackLng: {
+    //   default: ['en'],
+    // },
+    // nonExplicitSupportedLngs: true,
+    
+    // Add these for debugging in development
+    debug: process.env.NODE_ENV === 'development',
+    
+    // Make sure the namespace is correctly specified
+    ns: ['common'],
+    defaultNS: 'common',
+    
+    // Enable file system locales
+    localePath: './public/locales',
   };
