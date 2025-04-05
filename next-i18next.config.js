0c1acadf0e61c8c4562e6b2ed92dfdd4dceb1ed4
Lorenzo La Gorce <lorenzo.g@horizon-web.io>
2025-04-05 16:08:35 +0200

    Refactor next-i18next configuration to improve locale path resolution and remove non-standard options

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,22 +1,10 @@
-// next-i18next.config.js
 module.exports = {
     i18n: {
       defaultLocale: 'en',
       locales: ['en', 'es', 'fr', 'de'],
     },
-    // Remove these non-standard options
-    // fallbackLng: {
-    //   default: ['en'],
-    // },
-    // nonExplicitSupportedLngs: true,
-    
-    // Add these for debugging in development
-    debug: process.env.NODE_ENV === 'development',
-    
-    // Make sure the namespace is correctly specified
-    ns: ['common'],
-    defaultNS: 'common',
-    
-    // Enable file system locales
-    localePath: './public/locales',
+    localePath: typeof window === 'undefined'
+      ? require('path').resolve('./public/locales')
+      : '/locales',
   };
+  
